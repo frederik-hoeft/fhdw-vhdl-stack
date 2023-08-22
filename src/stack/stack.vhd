d7726cd474825f0e27d7f5cd31ae25fbd0b52d4c
frederik-hoeft <43813142+frederik-hoeft@users.noreply.github.com>
2023-08-22 16:54:59 +0200

    stack *should* work now?

--- src/stack/stack.vhd
+++ src/stack/stack.vhd
@@ -30,14 +30,14 @@ architecture stack_arch of stack is
     signal pop_data : std_logic_vector(7 downto 0);
     signal full_tmp, empty_tmp : std_logic;
 	 
-	 signal stack_pointer : integer;
-	 signal ram_enable : std_logic;
+    signal stack_pointer : integer;
+    signal ram_enable : std_logic;
 
     begin
         push_en <= push and not full_tmp;
         pop_en <= pop and not empty_tmp;
         clear_en <= clear;
-		  ram_enable <= push or pop or clear;
+        ram_enable <= push or pop or clear;
 
         RAMB4_S8_inst : RAMB4_S8 port map(
             we => push_en,
@@ -50,19 +50,28 @@ architecture stack_arch of stack is
 
         process(clk)
         begin
-            if rising_edge(clk) then
-				    if clear_en = '1'
-                if push_en = '1' then
-                    addr <= std_logic_vector(to_unsigned(to_integer(unsigned(addr)) + 1, addr'length));
+            if (rising_edge(clk)) then
+                if (clear_en = '1') then
+                    stack_pointer <= 0;
+                elsif (push_en = '1') then
+                    stack_pointer <= stack_pointer + 1;
+                elsif (pop_en = '1') then
+                    stack_pointer <= stack_pointer - 1;
                 end if;
-                if pop_en = '1' then
-                    addr <= std_logic_vector(to_unsigned(to_integer(unsigned(addr)) - 1, addr'length));
+                if (stack_pointer = 512) then
+                    full_tmp <= '1';
+                else 
+                    full_tmp <= '0';
                 end if;
-                full_tmp <= '1' when addr = "111111111" else '0';
-                empty_tmp <= '1' when addr = "000000000" else '0';
+                if (stack_pointer = 0) then
+                    empty_tmp <= '1';
+                else 
+                    empty_tmp <= '0';
+                end if;
             end if;
         end process;
 
+        addr <= std_logic_vector(to_unsigned(stack_pointer, addr'length));
         data <= pop_data;
         dout <= data;
         full <= full_tmp;
