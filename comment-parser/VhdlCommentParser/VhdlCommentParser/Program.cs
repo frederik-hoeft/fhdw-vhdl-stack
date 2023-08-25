86bfd03a7950205296224f0e0e5b7c86a090c594
frederik-hoeft <43813142+frederik-hoeft@users.noreply.github.com>
2023-08-25 18:42:33 +0200

    generate tests

--- comment-parser/VhdlCommentParser/VhdlCommentParser/Program.cs
+++ comment-parser/VhdlCommentParser/VhdlCommentParser/Program.cs
@@ -1,6 +1,10 @@
-﻿using System.Numerics;
-using System.Runtime.CompilerServices;
+﻿//#define METASTABILITY
+
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
 using System.Text.RegularExpressions;
+
+using static RegexThings;
 
 // Requires:
 // -> Csv file as input with the first line and first column being used for comments
@@ -25,53 +29,41 @@ if (File.Exists(inputFileName))
 {
     File.Delete(inputFileName);
 }
-using FileStream input = File.OpenWrite(inputFileName);
-using StreamWriter inputWriter = new(input);
+using FileStream inputStream = File.OpenWrite(inputFileName);
+using StreamWriter inputWriter = new(inputStream);
 
-string outputFileName = args[0].Replace(".csv", string.Empty) + "-outputs.txt";
-if (File.Exists(outputFileName))
+string expectedFileName = args[0].Replace(".csv", string.Empty) + "-expected.txt";
+if (File.Exists(expectedFileName))
 {
-    File.Delete(outputFileName);
+    File.Delete(expectedFileName);
 }
-using FileStream output = File.OpenWrite(outputFileName);
-using StreamWriter outputWriter = new(output);
+using FileStream expectedStream = File.OpenWrite(expectedFileName);
+using StreamWriter expectedWriter = new(expectedStream);
 
-string[] columnDefinitions = GetColumnsRegex().Split(lines[1]);
-Dictionary<int, int> dicColumnWidth = new();
+string[] columnDefinitions = GetColumnsRegex().Split(lines[0]).Skip(1).ToArray();
 
 // if something is written in line 2 we expect it to be the word width of the column
 // and that basically means that cell contents of the column are not binary digits
-// (we only allow decimal values and binary digits) so that means its decimal values
-for (int i = 0; i < columnDefinitions.Length; i++)
-{
-    if (!string.IsNullOrEmpty(columnDefinitions[i]))
-    {
-        dicColumnWidth.Add(i, Convert.ToInt32(columnDefinitions[i]));
-    }
-}
+FormatHandlerChain chain = new FormatHandlerChain()
+    .AddHandler<DecimalFormatHandler>()
+    .AddHandler<HexFormatHandler>()
+    .AddHandler<BinaryFormatHandler>()
+    .AddHandler<NopFormatHandler>()
+    .InitFrom(columnDefinitions);
 
 // ignore the first line when copying the content to a txt file
 for (int i = 2; i < lines.Length; i++)
 {
-    string[] inputsAndOutputs = GetEmptyColumnRegex().Split(lines[i]);
-    if (inputsAndOutputs.Length != 2)
-    {
-        Console.WriteLine("Could not detect input and output sections. Please provide input and output data separated by an empty column.");
-        return;
-    }
-    string inputs = inputsAndOutputs[0];
-    string outputs = inputsAndOutputs[1];
-
-    List<string> inputWordsOriginal = GetColumnsRegex().Split(inputs).Skip(1).ToList();
-    List<string> inputWordsConv = Conversions.DoConversions(inputWordsOriginal);
-
+    (string[] inputs, string[] expected) = chain.FormatLine(lines[i]);
+    
     // remove the first column of each line (first column is used for comments or similar)
-    string processedInputWords = string.Join(' ', inputWordsConv);
+    string processedInputWords = string.Join(' ', inputs);
     inputWriter.Write(processedInputWords);
     inputWriter.WriteLine();
 
     if (i == lines.Length - 1)
     {
+#if METASTABILITY
         // add more (basically dummy) inputs to the end while waiting for
         // input to output delay to complete tests
         // metastability shift (2 FFs)
@@ -80,38 +72,221 @@ for (int i = 2; i < lines.Length; i++)
             inputWriter.Write(processedInputWords);
             inputWriter.WriteLine();
         }
+#endif
         // base delay
-        inputWriter.Write(processedInputWords);
+        inputWriter.Write(AsDontCareLine(inputs));
     }
 
-    List<string> outputWordsOriginal = GetColumnsRegex().Split(outputs).ToList();
-    List<string> outputWordsConv = Conversions.DoConversions(outputWordsOriginal);
-    string processedOutputWords = string.Join(' ', outputWordsConv);
-    for (int j = 0; i == 1 && j < 3; j++)
+    string processedExpectedWords = string.Join(' ', expected);
+    for (int j = 0; i == 1 &&
+#if METASTABILITY
+        j < 3;
+#else
+        j < 1;
+#endif
+        j++)
     {
         // add more (basically dummy) outputs to the start while waiting for
         // input to output delay to start tests
         // metastability shift (2 FFs) + base delay
-        outputWriter.Write(processedOutputWords);
-        outputWriter.WriteLine();
+        expectedWriter.Write(AsDontCareLine(expected));
+        expectedWriter.WriteLine();
     }
-    outputWriter.Write(processedOutputWords);
+    expectedWriter.Write(processedExpectedWords);
     if (i < lines.Length - 1)
     {
-        outputWriter.WriteLine();
+        expectedWriter.WriteLine();
     }
 }
 
-partial class Program
+static string AsDontCareLine(IReadOnlyCollection<string> words)
 {
-    [GeneratedRegex(";|,")]
-    private static partial Regex GetColumnsRegex();
+    List<string> output = new();
+    foreach (string word in words)
+    {
+        output.Add(new string('-', word.Length));
+    }
+    return string.Join(' ', output);
 }
 
-partial class Program
+interface IFormatHandlerChain
 {
+    bool TryHandle(string format, out IFormatHandler? handler);
+}
+
+interface IFormatter
+{
+    string Format(string cellValue);
+}
+
+interface IFormatHandler : IFormatter
+{
+    static abstract bool TryCreateFor(string format, out IFormatHandler? handler);
+}
+
+delegate bool HandlerFactoryPointer(string format, out IFormatHandler? handler);
+
+partial class FormatHandlerChain : IFormatHandlerChain
+{
+    private readonly List<HandlerFactoryPointer> _handlerFactories = new();
+
+    public FormatHandlerChain AddHandler<T>() where T : IFormatHandler
+    {
+        _handlerFactories.Add(T.TryCreateFor);
+        return this;
+    }
+
+    private IFormatter[]? formatters;
+
+    public FormatHandlerChain InitFrom(string[] headerLine)
+    {
+        formatters ??= new IFormatter[headerLine.Length];
+        for (int i = 0; i < headerLine.Length; i++)
+        {
+            if (TryHandle(headerLine[i], out IFormatHandler? handler))
+            {
+                formatters[i] = handler;
+            }
+            else
+            {
+                throw new InvalidOperationException("RIP");
+            }
+        }
+        return this;
+    }
+
+    public (string[] Inputs, string[] Expected) FormatLine(string line)
+    {
+        string[] data = GetEmptyColumnRegex().Split(line);
+        if (data.Length != 2)
+        {
+            throw new FormatException("Could not detect input and output sections. Please provide input and output data separated by an empty column.");
+        }
+        string inputRaw = data[0];
+        string expectedRaw = data[1];
+
+        List<string> inputWordsOriginal = GetColumnsRegex().Split(inputRaw).Skip(1).ToList();
+        string[] inputs = new string[inputWordsOriginal.Count];
+        for (int i = 0; i < inputWordsOriginal.Count; i++)
+        {
+            inputs[i] = formatters![i].Format(inputWordsOriginal[i].Trim('\"'));
+        }
+        List<string> expectedWordsOriginal = GetColumnsRegex().Split(expectedRaw).ToList();
+        string[] expected = new string[expectedWordsOriginal.Count];
+        for(int i = 0; i < expectedWordsOriginal.Count; i++)
+        {
+            expected[i] = formatters![i + 1 + inputs.Length].Format(expectedWordsOriginal[i].Trim('\"'));
+        }
+        return (inputs, expected);
+    }
+
+    public bool TryHandle(string format, [NotNullWhen(true)] out IFormatHandler? handler)
+    {
+        foreach (HandlerFactoryPointer factory in _handlerFactories)
+        {
+            if (factory.Invoke(format, out handler!))
+            {
+                return true;
+            }
+        }
+        handler = null;
+        return false;
+    }
+}
+
+partial record class DecimalFormatHandler(int Length) : IFormatHandler
+{
+    public static bool TryCreateFor(string format, out IFormatHandler? handler)
+    {
+        // FORMAT::base_10:<length>
+        Match match = FormatRegex().Match(format);
+        if (match.Success)
+        {
+            int length = Convert.ToInt32(match.Groups["length"].Value);
+            handler = new DecimalFormatHandler(length);
+            return true;
+        }
+        handler = null;
+        return false;
+    }
+
+    public string Format(string cellValue) => 
+        cellValue.Equals("X") 
+            ? new string('-', Length) 
+            : Convert.ToString(Convert.ToInt32(cellValue), 2).PadLeft(Length, '0');
+
+    [GeneratedRegex("F::base_10:(?<length>[0-9]+)")]
+    private static partial Regex FormatRegex();
+}
+
+partial record class NopFormatHandler() : IFormatHandler
+{
+    public static bool TryCreateFor(string format, out IFormatHandler? handler)
+    {
+        handler = new NopFormatHandler();
+        return true;
+    }
+
+    public string Format(string cellValue) => cellValue;
+}
+
+partial record class HexFormatHandler(int Length) : IFormatHandler
+{
+    public static bool TryCreateFor(string format, out IFormatHandler? handler)
+    {
+        // F::base_16:<length>
+        Match match = FormatRegex().Match(format);
+        if (match.Success)
+        {
+            int length = Convert.ToInt32(match.Groups["length"].Value);
+            handler = new HexFormatHandler(length);
+            return true;
+        }
+        handler = null;
+        return false;
+    }
+
+    public string Format(string cellValue) => 
+        cellValue.Equals("X")
+            ? new string('-', Length) 
+            : Convert.ToString(int.Parse(cellValue, NumberStyles.HexNumber), 2).PadLeft(Length, '0');
+
+    [GeneratedRegex("F::base_16:(?<length>[0-9]+)")]
+    private static partial Regex FormatRegex();
+}
+
+partial record class BinaryFormatHandler(int Length) : IFormatHandler
+{
+    public static bool TryCreateFor(string format, out IFormatHandler? handler)
+    {
+        // F::base_2:<length>
+        Match match = FormatRegex().Match(format);
+        if (match.Success)
+        {
+            int length = Convert.ToInt32(match.Groups["length"].Value);
+            handler = new BinaryFormatHandler(length);
+            return true;
+        }
+        handler = null;
+        return false;
+    }
+
+    public string Format(string cellValue) =>
+        cellValue.Equals("X")
+            ? new string('-', Length)
+            : cellValue;
+
+    [GeneratedRegex("F::base_2:(?<length>[0-9]+)")]
+    private static partial Regex FormatRegex();
+}
+
+static partial class RegexThings
+{
+    [GeneratedRegex(";|,")]
+    public static partial Regex GetColumnsRegex();
+
     [GeneratedRegex(";;|,,")]
-    private static partial Regex GetEmptyColumnRegex();
+    public static partial Regex GetEmptyColumnRegex();
 }
 
 partial class Conversions
---
+++ tests/generate.py
@@ -0,0 +1,66 @@
+import csv
+
+# Define the header and initial rows (same as before)
+header = [
+    "format", "", "", "", "", "F::base_10:8", "",
+    "", "", "F::base_10:8"
+]
+pins_row = [
+    "pins", "push", "pop", "peek", "clear", "data_in", "", "full", "empty", "data_out"
+]
+clear_row1 = ["clear", "0", "0", "0", "1", "X", "", "0", "1", "X"]
+clear_row2 = ["clear", "0", "0", "0", "1", "X", "", "0", "1", "X"]
+
+# Initialize the test vector list (same as before)
+test_vectors = [header, pins_row, clear_row1, clear_row2]
+
+# Generate the push and verify push test vectors (same as before)
+for i in range(1, 254):
+    push_row = ["push " + str(i), "1", "0", "0", "0", str(i), "", "0", "0", "X"]
+    verify_push_row = ["verify push", "0", "0", "1", "0", "X", "", "0", "0", str(i)]
+    
+    test_vectors.extend([push_row, verify_push_row])
+
+# top entry causes full flag to be set
+test_vectors.append(["push 255", "1","0","0","0","255","","1","0","X"])
+test_vectors.append(["verify push", "0","0","1","0","X","","1","0","255"])
+test_vectors.append(["verify full", "0","0","0","0","X","","1","0","X"])
+
+# attempting to overflow the stack should be ignored
+test_vectors.append(["attempt overflow", "1","0","0","0","32","","1","0","X"])
+test_vectors.append(["verify didn't push", "0","0","1","0","X","","1","0","255"])
+
+# Simulate popping and verifying the top 100 most entries
+for i in range(1, 101):
+    pop_row = ["pop " + str(255 - i + 1), "0", "1", "0", "0", "X", "", "0", "0", str(255 - i + 1)]
+    test_vectors.append(pop_row)
+
+# clear stack
+test_vectors.append(["clear stack", "0","0","0","1","X","","0","1","X"])
+test_vectors.append(["verify clear", "0","0","0","0","X","","0","1","X"])
+
+# push/pop on clear stack
+test_vectors.append(["push 15", "1", "0", "0", "0", "15", "", "0", "0", "X"])
+test_vectors.append(["verify push", "0", "0", "1", "0", "X", "", "0", "0", "15"])
+test_vectors.append(["pop 15", "0", "1", "0", "0", "X", "", "0", "1", "15"])
+test_vectors.append(["verify clear", "0", "0", "0", "0", "X", "", "0", "1", "X"])
+
+# push/pop at the same time
+test_vectors.append(["push/pop at the same time", "1", "1", "0", "0", "15", "", "0", "0", "X"])
+
+# push/clear at the same time
+test_vectors.append(["push/clear at the same time", "1", "0", "0", "1", "15", "", "0", "1", "X"])
+
+# everything at the same time
+test_vectors.append(["everything at the same time", "1", "1", "1", "1", "15", "", "0", "1", "X"])
+
+# push/pop on clear stack
+test_vectors.append(["push 64", "1", "0", "0", "0", "64", "", "0", "0", "X"])
+test_vectors.append(["verify push", "0", "0", "1", "0", "X", "", "0", "0", "64"])
+test_vectors.append(["pop 64", "0", "1", "0", "0", "X", "", "0", "1", "64"])
+test_vectors.append(["verify clear", "0", "0", "0", "0", "X", "", "0", "1", "X"])
+
+# Write the test vectors to a CSV file
+with open("tb.csv", mode="w", newline="") as file:
+    writer = csv.writer(file)
+    writer.writerows(test_vectors)
